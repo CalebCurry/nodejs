4a81cc610e722255a3e2c03800dea9a3b849a1f9
Caleb Curry <caleb@calebcurry.com>
2023-01-04 10:13:24 -0500

    basic get and post request working w monogodb

--- src/app.js
+++ src/app.js
@@ -1,3 +1,78 @@
-const { v4: uuidv4 } = require('uuid');
+const express = require('express');
+const mongoose = require('mongoose');
+const Customer = require('./models/customer');
 
-console.log(uuidv4());
+const app = express();
+mongoose.set('strictQuery', false);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+if(process.env.NODE_ENV !== 'production') {
+	require('dotenv').config();
+}
+
+const PORT = process.env.PORT || 3000;
+const CONNECTION = process.env.CONNECTION;
+
+const customers = [
+	{
+		"name": "Caleb",
+		"industry": "music"	
+	},
+	{
+		"name": "John",
+		"industry": "networking"	
+	},
+	{
+		"name": "Sal",
+		"industry": "sports medicine"	
+	}
+];
+
+const customer = new Customer({
+	name: 'John',
+	industry: 'marketing'	
+});
+
+app.get('/', (req, res) => {
+	res.send("welcome!");	
+});
+
+app.get('/api/customers', async (req, res) => {
+	try{
+		const result = await Customer.find();
+		res.json({"customers": result});
+	} catch(e){
+		res.status(500).json({error: e.message});
+	}
+});
+
+app.post('/api/customers', async (req, res) => {
+	console.log(req.body);
+	const customer = new Customer(req.body);
+	try{
+		await customer.save();
+		res.status(201).json({customer}); 
+	} catch(e) {
+		res.status(400).json({error: e.message});
+	}
+});
+
+app.post('/', (req, res) => {
+	res.send('This is a post request!');	
+});
+
+const start = async() => {
+	try{
+		await mongoose.connect(CONNECTION);
+			
+		app.listen(PORT, () => {
+			console.log('App listening on port ' + PORT);
+		});
+	} catch(e) {
+		console.log(e.message)
+	}
+};
+
+start();
